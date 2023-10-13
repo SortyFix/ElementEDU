1399ebd0588956df91897e876617115e7093e6c4
Groldi <yonasnieder@gmail.com>
2023-10-13 17:12:21 +0200

    Implement user login response handling
    Enhanced the user login service to handle server responses. After posting the login data, the revised code now checks if the server's response status equals 200. If so, the system navigates the user to the home page, thereby enhancing UX. Moreover, HttpHeaders are now being utilized when making the HTTP post request to better conform to Angular best practices. Temporary navigation script added, to be replaced with user-specific home page.

--- EEDU-Frontend/src/app/user.service.ts
+++ EEDU-Frontend/src/app/user.service.ts
@@ -1,20 +1,28 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import { Router } from "@angular/router";
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserService {
-
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private router: Router) { }
 
     // NOT SECURE; PROTOTYPE!
     requestLogin(loginName: string, password: string){
         const body: {loginName: string, password: string } = {loginName, password};
+        const httpOptions = {
+            headers: new HttpHeaders({'Content-Type':  'application/json'}),
+            observe: 'response' as const
+        };
 
-        this.http.post("http://localhost:8080/user/login", body)
+        this.http.post("http://localhost:8080/user/login", body, httpOptions)
             .subscribe({
                 next: response => {
+                    if(response.status === 200){
+                        // Temporary navigation to default home page, TODO user specific pages
+                        this.router.navigate(["/home"]).then(r => console.log("Successful login; Switching to user dashboard."));
+                    }
                     console.log(response);
                 },
                 error: error => {
