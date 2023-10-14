a70dd66a5e20eba85656a69b6ee1dea555814c84
Ivo <chniucg@gmail.com>
2023-10-14 11:34:18 +0200

    Refactored file structure for clearer organization
    Several component files were moved to 'card' and 'user' directories to better organize the files according to their logical roles. The import paths in both app-routing.module.ts and app.module.ts were updated to reflect these changes in the file structure.

--- EEDU-Frontend/src/app/app-routing.module.ts
+++ EEDU-Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {LoginPageComponent} from './login-page/login-page.component';
-import {HomePageComponent} from "./home-page/home-page.component";
+import {LoginPageComponent} from './user/login-page/login-page.component';
+import {HomePageComponent} from "./card/home-page/home-page.component";
 import {CommonModule} from "@angular/common";
 
 const routes: Routes = [
--- EEDU-Frontend/src/app/app.module.ts
+++ EEDU-Frontend/src/app/app.module.ts
@@ -2,21 +2,21 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HeadBarComponent} from './head-bar/head-bar.component';
+import {HeadBarComponent} from './card/head-bar/head-bar.component';
 import {DashComponent} from './dash/dash.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './card/dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CardComponent } from './card/card.component';
-import { KlausurenCardComponent } from './klausuren-card/klausuren-card.component';
-import { ChatCardComponent } from './chat-card/chat-card.component';
-import { HausaufgabenCardComponent } from './hausaufgaben-card/hausaufgaben-card.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { KlausurenCardComponent } from './card/klausuren-card/klausuren-card.component';
+import { ChatCardComponent } from './card/chat-card/chat-card.component';
+import { HausaufgabenCardComponent } from './card/hausaufgaben-card/hausaufgaben-card.component';
+import { LoginPageComponent } from './user/login-page/login-page.component';
+import { HomePageComponent } from './card/home-page/home-page.component';
 import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
