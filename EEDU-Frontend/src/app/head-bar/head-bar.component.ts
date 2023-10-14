23b8a7d196768b7ee61f9a7f3396b8e8020e6584
Ivo <chniucg@gmail.com>
2023-10-14 18:11:30 +0200

    Showcased bootstrap and fixed some minor issues

--- EEDU-Frontend/src/app/head-bar/head-bar.component.ts
+++ EEDU-Frontend/src/app/head-bar/head-bar.component.ts
@@ -2,33 +2,26 @@ import {ChangeDetectorRef, Component} from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
-    selector: 'app-head-bar',
-    templateUrl: './head-bar.component.html',
-    styleUrls: ['./head-bar.component.scss']
+    selector: 'app-head-bar', templateUrl: './head-bar.component.html', styleUrls: ['./head-bar.component.scss']
 })
 export class HeadBarComponent {
     time: string = '';
-    today: Date = new Date();
 
     constructor(private cdr: ChangeDetectorRef, private router: Router) {
+        this.update(); // Ensure date is shown as fast as possible
         setInterval(() => {
-            // Show date on header
-            this.today = new Date();
-            this.time = parseNumber(this.today.getDate()) + "."
-                + parseNumber(this.today.getMonth() + 1) + "."
-                + parseNumber(this.today.getFullYear());
-            // Ensure date update
+            this.update();
             this.cdr.detectChanges();
         }, 1000);
-
-        function parseNumber(date: number) {
-            if (date < 10) {
-                return "0" + date;
-            }
-            return date;
-        }
     }
-    switchPage(){
+
+    private update() {
+        let today = new Date();
+        // Ensure date update
+        this.time = today.getDate().toString().padStart(2, '0') + "." + (today.getMonth() + 1).toString().padStart(2, '0') + "." + today.getFullYear();
+    }
+
+    public switchPage() {
         this.router.navigate(["/user-login"]).then(() => console.log('Successfully switched page.'));
     }
 }
