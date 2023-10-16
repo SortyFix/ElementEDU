cfa885245e1773c71fdbba50aa629f5d20bc8d1f
Ivo <chniucg@gmail.com>
2023-10-16 14:27:05 +0200

    Refactor CORS handling for user login
    Removed @CrossOrigin annotation from loginUser() in UserController.java as this method of handling Cross Origin Requests was deemed inappropriate. Instead, configured CORS at a global level within SecurityConfig.java to ensure availability across all endpoints, increasing the application's flexibility and maintainability.

--- EEDU-Backend/src/main/java/de/gaz/eedu/security/SecurityConfig.java
+++ EEDU-Backend/src/main/java/de/gaz/eedu/security/SecurityConfig.java
@@ -12,20 +12,31 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebSecurity
-@EnableMethodSecurity
-@Configuration @AllArgsConstructor
-@CrossOrigin(origins = "*")
-public class SecurityConfig {
+@EnableWebSecurity @EnableMethodSecurity @EnableWebMvc @Configuration @AllArgsConstructor public class SecurityConfig
+{
 
     private final UserService userService;
 
-    @Bean public SecurityFilterChain filterChain(@NotNull HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .addFilterBefore(new JwtAuthorizationFilter(userService), UsernamePasswordAuthenticationFilter.class)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+    @Bean public @NotNull SecurityFilterChain filterChain(@NotNull HttpSecurity http) throws Exception
+    {
+        http.csrf(AbstractHttpConfigurer::disable).addFilterBefore(new JwtAuthorizationFilter(userService), UsernamePasswordAuthenticationFilter.class).authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
         return http.build();
     }
+
+    @Bean public @NotNull WebMvcConfigurer corsMappingConfigurer()
+    {
+        return new WebMvcConfigurer()
+        {
+            @Override public void addCorsMappings(@NotNull CorsRegistry registry)
+            {
+                registry.addMapping("/").allowedOrigins("*");
+            }
+        };
+    }
+
 }
