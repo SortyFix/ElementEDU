57934ba5c7ffeec30694ba02cc4a0cdcc50e351a
Groldi <yonasnieder@gmail.com>
2023-10-18 17:24:53 +0200

    Added colors to ThemeEntity

--- EEDU-Backend/src/main/java/de/gaz/eedu/user/theming/ThemeEntity.java
+++ EEDU-Backend/src/main/java/de/gaz/eedu/user/theming/ThemeEntity.java
@@ -7,8 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 
-import java.awt.*;
-
 @Entity public class ThemeEntity
 {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,6 +14,26 @@ import java.awt.*;
     @OneToOne
     private UserEntity userEntity;
     private String name;
-    private int[] colors;
+    private int[] green = {
+            0x077f1b,
+            399949,
+            0x6ab276,
+            0x9ccca4,
+            0xcde5d1
+    },
+    blue = {
+            0x222d65,
+            0x4e5784,
+            0x7a81a3,
+            0xa7abc1,
+            0xd3d5e0
+    },
+    red = {
+            0xa53c26,
+            0xb76351,
+            0xc98a7d,
+            0xdbb1a8,
+            0xedd8d4
+    };
 
 }
