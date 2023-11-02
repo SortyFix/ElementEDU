e3bd83a03cb8222dc943bcd6df57298b83e410a6
Groldi <yonasnieder@gmail.com>
2023-11-02 19:58:19 +0100

    Created new ThemeController with a set and create function, the second one being withheld for admins.

--- EEDU-Backend/src/main/java/de/gaz/eedu/user/theming/ThemeController.java
+++ EEDU-Backend/src/main/java/de/gaz/eedu/user/theming/ThemeController.java
@@ -1,24 +1,20 @@
 package de.gaz.eedu.user.theming;
 
-import de.gaz.eedu.user.UserEntity;
+import de.gaz.eedu.exception.NameOccupiedException;
 import de.gaz.eedu.user.UserService;
 import jakarta.annotation.security.PermitAll;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.apache.catalina.User;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpResponse;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/user", method = RequestMethod.POST)
@@ -31,25 +27,23 @@ public class ThemeController
     @Getter(AccessLevel.PROTECTED)
     private final UserService userService;
 
-    @PermitAll @GetMapping("/me/{id}/theme/set") public ResponseEntity<ThemeEntity> setTheme(@NotNull @PathVariable Long id, @RequestBody ThemeStyle themeStyle){
+    @PermitAll @PostMapping("/me/{id}/theme/set") public ResponseEntity<ThemeEntity> setTheme(@PathVariable @NotNull Long id, @RequestBody String name){
         logger.info("The server has recognized an incoming theme set request.");
-        ThemeEntity themeEntity = new ThemeEntity();
-        switch(themeStyle){
-            case FIRE:
-                if(userService.loadEntityByID(id).isPresent()){
-                    themeEntity.setBackgroundColor(0xa53c26);
-                    themeEntity.setWidgetColor(0xedd8d4);
-                    themeEntity.setTextColor(0xFFFFFF);
-                    themeEntity.setName("Fire");
-                    // orElse(new UserEntity) will never be thrown.
-                    themeEntity.setUserEntity(userService.loadEntityByID(id).orElse(new UserEntity()));
-                    return ResponseEntity.ok(themeEntity);
-                }
-                else{
-                    return ResponseEntity.badRequest().build();
-                }
-            default:
-                return ResponseEntity.badRequest().build();
+        try{
+            ThemeEntity themeEntity = themeService.loadEntityByName(name).orElseThrow(IllegalArgumentException::new);
+            themeEntity.setUserEntity(userService.loadEntityByID(id).orElseThrow(IllegalArgumentException::new));
+            return ResponseEntity.ok(themeEntity);
+        }
+        catch(IllegalArgumentException illegalArgumentException){
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+    @PreAuthorize("hasAuthority('ADMIN')") @PostMapping("/theme/create") public @NotNull ResponseEntity<ThemeEntity> createTheme(@NotNull @RequestBody ThemeCreateModel themeCreateModel){
+        try {
+            return ResponseEntity.ok(themeService.createEntity(themeCreateModel));
+        }
+        catch(NameOccupiedException nameOccupiedException) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
         }
     }
 }
