3d3c4ebc022cd67b4f7325a622eddc3a8de28a46
Groldi <yonasnieder@gmail.com>
2023-11-13 22:34:11 +0100

    Added some Javadoc
    - Added some javadoc to FileService

--- EEDU-Backend/src/main/java/de/gaz/eedu/user/file/FileService.java
+++ EEDU-Backend/src/main/java/de/gaz/eedu/user/file/FileService.java
@@ -11,6 +11,15 @@ import java.util.Set;
 {
     FileRepository fileRepository;
 
+    /**
+     * Checks if file is empty or contains malicious files.
+     * Uses ClamAV for malware detection.
+     * @param file
+     * <code>MultipartFile</code>
+     * @return <code>Boolean</code> <br>
+     * True: File is usable <br>
+     * False: File is unusable
+     */
     public @NotNull Boolean isFileValid(@NotNull MultipartFile file)
     {
         // TODO: Implement antivirus check (with ClamAV?)
@@ -23,6 +32,13 @@ import java.util.Set;
         return fileRepository.findById(id);
     }
 
+    /**
+     * Returns a <code>{@literal Set<}{@link FileEntity}{@literal >}</code> containing all FileEntities
+     * matching <code>tag</code>
+     * @param tag
+     * <code>String</code>
+     * @return <code>{@literal Set<}{@link FileEntity}{@literal >}</code>
+     */
     public @NotNull Set<FileEntity> loadEntitiesByTag(@NotNull String tag)
     {
         return fileRepository.findFileEntitiesByTags(tag);
@@ -36,6 +52,14 @@ import java.util.Set;
         }));
     }
 
+    /**
+     * Deletes the {@link FileEntity} that holds the given <cold>id</cold>
+     * parameter value as file id.
+     * @param id
+     * @return boolean <br>
+     * True: Successfully deleted <br>
+     * False: Couldn't find {@link FileEntity} with id.
+     */
     public boolean delete(@NotNull Long id){
         return fileRepository.findById(id).map(fileEntity ->
         {
