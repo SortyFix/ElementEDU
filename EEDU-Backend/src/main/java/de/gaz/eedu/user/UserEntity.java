d1cd9dd7faa5a4a5810fbcc8570bc22f0a274bcd
Ivo Quiring <falk.quiring@gmail.com>
2024-01-30 18:26:57 +0100

    important changes

--- EEDU-Backend/src/main/java/de/gaz/eedu/user/UserEntity.java
+++ EEDU-Backend/src/main/java/de/gaz/eedu/user/UserEntity.java
@@ -104,7 +104,7 @@ import java.util.stream.Stream;
 
     @Override public boolean isAccountNonLocked()
     {
-        return !locked;
+        return !isLocked();
     }
 
     @Override public boolean isEnabled()
