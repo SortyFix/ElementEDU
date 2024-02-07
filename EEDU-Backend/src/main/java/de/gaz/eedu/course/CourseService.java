8a06590e755b2017115979d93d458869f6a85020
Ivo Quiring <falk.quiring@gmail.com>
2024-02-07 21:34:23 +0100

    Implemented abilities when creating courses or classes

--- EEDU-Backend/src/main/java/de/gaz/eedu/course/CourseService.java
+++ EEDU-Backend/src/main/java/de/gaz/eedu/course/CourseService.java
@@ -1,11 +1,15 @@
 package de.gaz.eedu.course;
 
+import de.gaz.eedu.course.classroom.ClassRoomRepository;
 import de.gaz.eedu.course.model.CourseCreateModel;
 import de.gaz.eedu.course.model.CourseModel;
 import de.gaz.eedu.course.subjects.SubjectService;
 import de.gaz.eedu.entity.EntityService;
 import de.gaz.eedu.exception.CreationException;
+import de.gaz.eedu.exception.EntityUnknownException;
 import de.gaz.eedu.exception.NameOccupiedException;
+import de.gaz.eedu.user.UserEntity;
+import de.gaz.eedu.user.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -13,19 +17,24 @@ import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor @Service
+import java.util.List;
+
+@RequiredArgsConstructor @Service @Getter(AccessLevel.PROTECTED)
 public class CourseService implements EntityService<CourseRepository, CourseEntity, CourseModel, CourseCreateModel>
 {
-    private final CourseRepository courseRepository;
-    @Getter(AccessLevel.PROTECTED) private final SubjectService subjectService;
+    @Getter(AccessLevel.NONE) private final CourseRepository courseRepository;
+    private final SubjectService subjectService;
+
+    private final UserRepository userRepository;
+    private final ClassRoomRepository classRoomRepository;
 
     @Override public @NotNull CourseRepository getRepository()
     {
         return courseRepository;
     }
 
-    @Transactional @Override
-    public @NotNull CourseEntity createEntity(@NotNull CourseCreateModel model) throws CreationException
+    @Transactional @Override public @NotNull CourseEntity createEntity(
+            @NotNull CourseCreateModel model) throws CreationException
     {
         if (getRepository().existsByName(model.name()))
         {
@@ -34,6 +43,22 @@ public class CourseService implements EntityService<CourseRepository, CourseEnti
 
         return saveEntity(model.toEntity(new CourseEntity(), (entity) ->
         {
+            // attach users
+            if(model.users().length > 0)
+            {
+                List<UserEntity> userEntities = getUserRepository().findAllById(List.of(model.users()));
+                entity.attachUsers(userEntities.toArray(UserEntity[]::new));
+            }
+
+            // assign class
+            if (model.classId() != null)
+            {
+                getClassRoomRepository().findById(model.classId()).ifPresentOrElse(entity::assignClassRoom, () ->
+                {
+                    throw new EntityUnknownException(model.classId());
+                });
+            }
+
             // add to subject
             entity.setSubject(getSubjectService().loadEntityByIDSafe(model.subjectId()));
             return entity;
--- EEDU-Backend/src/main/java/de/gaz/eedu/course/classroom/ClassRoomService.java
+++ EEDU-Backend/src/main/java/de/gaz/eedu/course/classroom/ClassRoomService.java
@@ -7,6 +7,8 @@ import de.gaz.eedu.course.classroom.model.ClassRoomModel;
 import de.gaz.eedu.entity.EntityService;
 import de.gaz.eedu.exception.CreationException;
 import de.gaz.eedu.exception.NameOccupiedException;
+import de.gaz.eedu.user.UserEntity;
+import de.gaz.eedu.user.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @RequiredArgsConstructor @Service @Getter
@@ -21,20 +24,40 @@ public class ClassRoomService implements EntityService<ClassRoomRepository, Clas
 {
     @Getter(AccessLevel.NONE) private final ClassRoomRepository classRoomRepository;
     private final CourseService courseService;
+    private final UserRepository userRepository;
 
     @Override public @NotNull ClassRoomRepository getRepository()
     {
         return classRoomRepository;
     }
 
-    @Transactional @Override public @NotNull ClassRoomEntity createEntity(@NotNull ClassRoomCreateModel model) throws CreationException
+    @Transactional @Override public @NotNull ClassRoomEntity createEntity(
+            @NotNull ClassRoomCreateModel model) throws CreationException
     {
         if (getRepository().existsByName(model.name()))
         {
             throw new NameOccupiedException(model.name());
         }
 
-        return saveEntity(model.toEntity(new ClassRoomEntity()));
+        ClassRoomEntity classRoomEntity = saveEntity(model.toEntity(new ClassRoomEntity(), (entity) ->
+        {
+            // attach users
+            if (model.users().length > 0)
+            {
+                List<UserEntity> userEntities = getUserRepository().findAllById(List.of(model.users()));
+                entity.attachStudents(userEntities.toArray(UserEntity[]::new));
+            }
+
+            return entity;
+        }));
+
+        // add classroom to courses after entity was saved.
+        // assign courses
+        List<CourseEntity> courseEntities = getCourseService().getRepository().findAllById(List.of(model.courses()));
+        courseEntities.forEach(courseEntity -> courseEntity.assignClassRoom(classRoomEntity));
+        getCourseService().saveEntity(courseEntities);
+
+        return classRoomEntity;
     }
 
     @Override public void deleteRelations(@NotNull ClassRoomEntity entry)
