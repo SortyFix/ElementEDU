38d853c87fa7e098cdac166666062361d5b1ef26
Ivo Quiring <falk.quiring@gmail.com>
2024-05-06 20:57:05 +0200

    Implemented rudimentary file upload to AppointmentEntryEntity
    Note that I still need to think about how to actually implement the homework system.

--- EEDU-Backend/src/main/java/de/gaz/eedu/course/appointment/entry/AppointmentEntryEntity.java
+++ EEDU-Backend/src/main/java/de/gaz/eedu/course/appointment/entry/AppointmentEntryEntity.java
@@ -3,6 +3,9 @@ package de.gaz.eedu.course.appointment.entry;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import de.gaz.eedu.course.appointment.CourseAppointmentEntity;
 import de.gaz.eedu.entity.model.EntityObject;
+import de.gaz.eedu.file.FileEntity;
+import de.gaz.eedu.file.exception.MaliciousFileException;
+import de.gaz.eedu.user.UserEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -11,12 +14,11 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
-@Entity @NoArgsConstructor @Getter @Setter public class AppointmentEntryEntity implements EntityObject
+@Slf4j @Entity @NoArgsConstructor @Getter @Setter public class AppointmentEntryEntity implements EntityObject
 {
     @Setter(AccessLevel.NONE) @Id private long id;
     private String description, homework;
@@ -28,8 +30,23 @@ import java.io.IOException;
         this.id = id;
     }
 
-    public void uploadHomework(@NotNull MultipartFile... files) throws IOException
+    public void uploadHomework(@NotNull UserEntity user, @NotNull MultipartFile... files) throws MaliciousFileException
     {
-        //TODO
+        if (!getCourseAppointment().getCourse().getUsers().contains(user))
+        {
+            String warnMessage = "Uploading files from user {} to appointment entry {}. But {} is not a part of the course.";
+            log.warn(warnMessage, user.getId(), getId(), user.getId());
+        }
+
+        String uploadPath = uploadPath(user);
+        FileEntity fileEntity = getCourseAppointment().getCourse().getRepository();
+        fileEntity.uploadBatch(uploadPath, files);
+
+        log.info("User {} has uploaded files to appointment entry {}", user.getId(), getId());
+    }
+
+    private @NotNull String uploadPath(@NotNull UserEntity user)
+    {
+        return String.format("%s", user.getId());
     }
 }
