ed269432fcb20ac3b470bff39f5b9f55ce45fd9f
Ivo Quiring <falk.quiring@gmail.com>
2024-05-26 13:38:33 +0200

    Removed garbage code

--- EEDU-Frontend/src/app/app-routing.module.ts
+++ EEDU-Frontend/src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {LoginPageComponent} from './user/login-page/login-page.component';
-import {HomePageComponent} from "./card/home-page/home-page.component";
 import {CommonModule} from "@angular/common";
 
 const routes: Routes = [
     // set 'home' as main page if URL is /localhost:4200/
     {path: '', redirectTo: '/home', pathMatch: "full"},
-    {path: 'user-login', component: LoginPageComponent},
-    {path: 'home', component: HomePageComponent}
 ];
 
 @NgModule({
--- EEDU-Frontend/src/app/app.module.ts
+++ EEDU-Frontend/src/app/app.module.ts
@@ -2,38 +2,18 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HeadBarComponent} from './head-bar/head-bar.component';
-import {DashComponent} from './dash/dash.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashboardComponent } from './card/dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { CardComponent } from './card/card.component';
-import { KlausurenCardComponent } from './card/klausuren-card/klausuren-card.component';
-import { ChatCardComponent } from './card/chat-card/chat-card.component';
-import { HausaufgabenCardComponent } from './card/hausaufgaben-card/hausaufgaben-card.component';
-import { LoginPageComponent } from './user/login-page/login-page.component';
-import { HomePageComponent } from './card/home-page/home-page.component';
 import { HttpClientModule } from "@angular/common/http";
-import { SettingsComponent } from './user/settings/settings.component';
 import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
     declarations: [
         AppComponent,
-        HeadBarComponent,
-        DashComponent,
-        DashboardComponent,
-        CardComponent,
-        KlausurenCardComponent,
-        ChatCardComponent,
-        HausaufgabenCardComponent,
-        LoginPageComponent,
-        HomePageComponent,
-        SettingsComponent
     ],
     imports: [
         BrowserModule,
