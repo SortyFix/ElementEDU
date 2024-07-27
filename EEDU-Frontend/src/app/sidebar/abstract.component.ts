ac1c84296d91bc1eddc6e3507af203d1ce30f5cb
sortyfix <yonasnieder@gmail.com>
2024-07-27 17:32:42 +0200

    Implemented routing

--- EEDU-Frontend/src/app/sidebar/abstract.component.ts
+++ EEDU-Frontend/src/app/sidebar/abstract.component.ts
@@ -8,16 +8,25 @@ import {Router} from "@angular/router";
 })
 
 export class AbstractComponent {
+    private _mobile: boolean = false;
+
     constructor(public router: Router) { }
 
     sidebar_buttons = [
-        {title:'Georg-August-Zinn-Schule', icon_name: 'school', route:'school'},
         {title:'Dashboard', icon_name: 'dashboard', route:'dashboard'},
         {title:'Courses', icon_name:'book_5', route: 'courses'},
         {title:'Timetable', icon_name:'calendar_view_week', route: 'timetable'},
-        {title:'Calendar', icon_name: 'calendar_month', route: 'calendar'},
         {title:'News', icon_name: 'newspaper', route: 'news'},
         {title:'Chat', icon_name: 'forum', route: 'chat'},
         {title:'Settings', icon_name: 'settings', route: 'settings'}
     ]
+
+    private checkScreenSize()
+    {
+        this._mobile = window.innerWidth <= 600;
+    }
+
+    get mobile(): boolean {
+        return this._mobile;
+    }
 }
