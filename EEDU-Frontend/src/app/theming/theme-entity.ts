2f41033a05a3ab4ae2c99fc31a533af9fb82df53
SortyFix <yonasnieder@gmail.com>
2024-09-26 16:20:07 +0200

    Adjusted theme-entity.ts to fit new theme persistence logic

--- EEDU-Frontend/src/app/theming/theme-entity.ts
+++ EEDU-Frontend/src/app/theming/theme-entity.ts
@@ -1,16 +1,26 @@
 export class ThemeEntity {
     public readonly id: bigint;
     public readonly name: string;
-    public readonly backgroundColor: number;
-    public readonly widgetColor: number;
-    public readonly textColor: number;
+
+    public readonly backgroundColor_r: number;
+    public readonly backgroundColor_g: number;
+    public readonly backgroundColor_b: number;
+
+    public readonly widgetColor_r: number;
+    public readonly widgetColor_g: number;
+    public readonly widgetColor_b: number;
 
     constructor(id: bigint, name: string, backgroundColor: number, widgetColor: number, textColor: number)
     {
         this.id = id;
         this.name = name;
-        this.backgroundColor = backgroundColor;
-        this.widgetColor = widgetColor;
-        this.textColor = textColor;
+
+        this.backgroundColor_r = backgroundColor;
+        this.backgroundColor_g = backgroundColor;
+        this.backgroundColor_b = backgroundColor;
+
+        this.widgetColor_r = widgetColor;
+        this.widgetColor_g = widgetColor;
+        this.widgetColor_b = widgetColor;
     }
 }
