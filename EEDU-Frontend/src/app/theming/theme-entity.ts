a6f04a8149665183569046e1bb36399f95028968
SortyFix <yonasnieder@gmail.com>
2024-09-26 16:22:18 +0200

    Fixed incomplete theme-entity.ts

--- EEDU-Frontend/src/app/theming/theme-entity.ts
+++ EEDU-Frontend/src/app/theming/theme-entity.ts
@@ -10,17 +10,17 @@ export class ThemeEntity {
     public readonly widgetColor_g: number;
     public readonly widgetColor_b: number;
 
-    constructor(id: bigint, name: string, backgroundColor: number, widgetColor: number, textColor: number)
+    constructor(id: bigint, name: string, backgroundColor_r: number, backgroundColor_g: number, backgroundColor_b: number, widgetColor_r: number, widgetColor_g: number, widgetColor_b: number)
     {
         this.id = id;
         this.name = name;
 
-        this.backgroundColor_r = backgroundColor;
-        this.backgroundColor_g = backgroundColor;
-        this.backgroundColor_b = backgroundColor;
+        this.backgroundColor_r = backgroundColor_r;
+        this.backgroundColor_g = backgroundColor_g;
+        this.backgroundColor_b = backgroundColor_b;
 
-        this.widgetColor_r = widgetColor;
-        this.widgetColor_g = widgetColor;
-        this.widgetColor_b = widgetColor;
+        this.widgetColor_r = widgetColor_r;
+        this.widgetColor_g = widgetColor_g;
+        this.widgetColor_b = widgetColor_b;
     }
 }
