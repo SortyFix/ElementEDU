8dc6e0b4a3319a5b7f864a1800041665fad2be25
Ivo Quiring <falk.quiring@gmail.com>
2024-10-28 00:01:00 +0100

    Wrote javadoc
    Signed-off-by: Ivo Quiring <falk.quiring@gmail.com>

--- EEDU-Backend/src/main/java/de/gaz/eedu/user/group/GroupController.java
+++ EEDU-Backend/src/main/java/de/gaz/eedu/user/group/GroupController.java
@@ -5,24 +5,46 @@ import de.gaz.eedu.exception.CreationException;
 import de.gaz.eedu.user.UserService;
 import de.gaz.eedu.user.group.model.GroupCreateModel;
 import de.gaz.eedu.user.group.model.GroupModel;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller for managing group-related operations for users.
+ * <p>
+ * This controller provides endpoints for creating, deleting, retrieving, attaching, detaching, granting, and
+ * revoking privileges for groups.
+ * <p>
+ * Accessible only to users with appropriate privileges as specified for each endpoint.
+ *
+ * @author Ivo Quiring
+ */
 @RestController
 @RequestMapping("/api/v1/user/group")
 @RequiredArgsConstructor
+@Getter(AccessLevel.PROTECTED)
 public class GroupController extends EntityController<GroupService, GroupModel, GroupCreateModel>
 {
-    private final GroupService groupService;
+    private final GroupService entityService;
 
-    @Override protected @NotNull GroupService getEntityService()
-    {
-        return groupService;
-    }
-
+    /**
+     * Handles the attachment of groups to a specified user.
+     * <p>
+     * This method processes requests to attach one or more groups to a specified user, identified by the provided
+     * {@code userid}. The groupIds are provided in the request body, and the groups are loaded and attached to
+     * the user upon successful processing. This endpoint is restricted to users with appropriate privileges.
+     * <p>
+     * This endpoint is accessible only to users with any of the following authorities: {@code ${privilege.user.group.attach}}
+     * or {@code ${privilege.user.all}}.
+     *
+     * @param user   the unique identifier of the target user, provided as a path variable.
+     * @param groups an array of group IDs to attach to the specified user, provided in the request body.
+     *               Must not be null.
+     */
     @PreAuthorize("hasAnyAuthority(${privilege.user.group.attach}, ${privilege.user.all})")
     @PostMapping("/{user}/attach")
     public void attachGroups(@PathVariable long user, @RequestBody @NotNull Long... groups)
@@ -32,6 +54,20 @@ public class GroupController extends EntityController<GroupService, GroupModel, 
         userService.loadEntityByIDSafe(user).attachGroups(userService, entities);
     }
 
+    /**
+     * Handles the detachment of groups from a specified user.
+     * <p>
+     * This method processes requests to detach one or more groups from a specified user, identified by the
+     * provided {@code userid}. The groupIds are provided in the request body, and the specified groups are
+     * detached from the user upon successful processing. This endpoint is restricted to users with appropriate privileges.
+     * <p>
+     * This endpoint is accessible only to users with any of the following authorities: {@code ${privilege.user.group.detach}}
+     * or {@code ${privilege.user.all}}.
+     *
+     * @param user   the unique identifier of the target user, provided as a path variable.
+     * @param groups an array of group IDs to detach from the specified user, provided in the request body.
+     *               Must not be null.
+     */
     @PreAuthorize("hasAnyAuthority(${privilege.user.group.detach}, ${privilege.user.all})")
     @PostMapping("/{user}/detach")
     public void detachGroups(@PathVariable long user, @RequestBody @NotNull Long... groups)
@@ -40,6 +76,21 @@ public class GroupController extends EntityController<GroupService, GroupModel, 
         userService.loadEntityByIDSafe(user).detachGroups(userService, groups);
     }
 
+    /**
+     * Handles the creation of a new group.
+     * <p>
+     * This method processes requests to create a new group using the provided {@link GroupCreateModel}. Upon
+     * successful creation, a {@link ResponseEntity} containing the created {@link GroupModel} is returned. This
+     * endpoint is restricted to users with appropriate privileges.
+     * <p>
+     * This endpoint is accessible only to users with any of the following authorities: {@code ${privilege.group.create}}
+     * or {@code ${privilege.group.all}}.
+     *
+     * @param model the {@link GroupCreateModel} containing details for the new group, provided in the request body.
+     *              Must not be null.
+     * @return a {@link ResponseEntity} containing the created {@link GroupModel} upon successful creation.
+     * @throws CreationException if an error occurs during the creation of the group.
+     */
     @PreAuthorize("hasAnyAuthority(${privilege.group.create}, ${privilege.group.all})") @PostMapping("/create")
     @Override
     public @NotNull ResponseEntity<GroupModel> create(@NotNull @RequestBody GroupCreateModel model) throws CreationException
@@ -47,12 +98,38 @@ public class GroupController extends EntityController<GroupService, GroupModel, 
         return super.create(model);
     }
 
+    /**
+     * Handles the deletion of a group.
+     * <p>
+     * This method processes requests to delete a group specified by the provided {@code id}. The deletion
+     * is performed only if the user has the required authority. This endpoint is restricted to users with
+     * appropriate privileges.
+     * <p>
+     * This endpoint is accessible only to users with any of the following authorities: {@code ${privilege.group.delete}}
+     * or {@code ${privilege.group.all}}.
+     *
+     * @param id the unique identifier of the group to be deleted, provided as a path variable. Must not be null.
+     * @return {@code true} if the group was successfully deleted; otherwise, {@code false}.
+     */
     @PreAuthorize("hasAnyAuthority(${privilege.group.delete}, ${privilege.group.all})") @DeleteMapping("/delete/{id}")
     @Override public @NotNull Boolean delete(@PathVariable @NotNull Long id)
     {
         return super.delete(id);
     }
 
+    /**
+     * Retrieves data for a specific group.
+     * <p>
+     * This method processes requests to retrieve information about a group specified by the provided {@code id}.
+     * The requested group's details are returned in a {@link ResponseEntity} containing the {@link GroupModel}.
+     * This endpoint is restricted to users with appropriate privileges.
+     * <p>
+     * This endpoint is accessible only to users with any of the following authorities: {@code ${privilege.group.get}}
+     * or {@code ${privilege.group.all}}.
+     *
+     * @param id the unique identifier of the group to retrieve, provided as a path variable. Must not be null.
+     * @return a {@link ResponseEntity} containing the {@link GroupModel} of the specified group.
+     */
     @PreAuthorize("hasAnyAuthority(${privilege.group.get}, ${privilege.group.all})") @GetMapping("/get/{id}") @Override
     public @NotNull ResponseEntity<GroupModel> getData(@PathVariable @NotNull Long id)
     {
--- EEDU-Backend/src/main/java/de/gaz/eedu/user/privileges/PrivilegeController.java
+++ EEDU-Backend/src/main/java/de/gaz/eedu/user/privileges/PrivilegeController.java
@@ -11,12 +11,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller for managing privilege-related operations for user groups.
+ * <p>
+ * This controller provides endpoints for creating, deleting, retrieving, granting, and revoking privileges.
+ * <p>
+ * Accessible only to users with appropriate privileges as specified for each endpoint.
+ *
+ * @author Ivo Quiring
+ */
 @RestController
 @RequestMapping("/api/v1/user/group/privilege")
 @RequiredArgsConstructor
 public class PrivilegeController extends EntityController<PrivilegeService, PrivilegeModel, PrivilegeCreateModel>
 {
-
     private final PrivilegeService privilegeService;
 
     @Override protected @NotNull PrivilegeService getEntityService()
@@ -24,6 +32,20 @@ public class PrivilegeController extends EntityController<PrivilegeService, Priv
         return privilegeService;
     }
 
+    /**
+     * Handles the creation of a new privilege.
+     * <p>
+     * This method processes requests to create a new privilege using the provided {@link PrivilegeCreateModel}.
+     * Upon successful creation, a {@link ResponseEntity} containing the created {@link PrivilegeModel} is returned.
+     * This endpoint is restricted to users with appropriate privileges.
+     * <p>
+     * This endpoint is accessible only to users with any of the following authorities: {@code ${privilege.privilege.create}}
+     * or {@code ${privilege.privilege.all}}.
+     *
+     * @param model the {@link PrivilegeCreateModel} containing details for the new privilege, provided in the request body.
+     *              Must not be null.
+     * @return a {@link ResponseEntity} containing the created {@link PrivilegeModel} upon successful creation.
+     */
     @PreAuthorize("hasAnyAuthority(${privilege.privilege.create}, ${privilege.privilege.all})") @PostMapping("/create")
     @Override
     public @NotNull ResponseEntity<PrivilegeModel> create(@NotNull @RequestBody PrivilegeCreateModel model) throws CreationException
@@ -31,19 +53,60 @@ public class PrivilegeController extends EntityController<PrivilegeService, Priv
         return super.create(model);
     }
 
+    /**
+     * Handles the deletion of a privilege.
+     * <p>
+     * This method processes requests to delete a privilege specified by the provided {@code id}. The deletion
+     * is performed only if the user has the required authority. This endpoint is restricted to users with
+     * appropriate privileges.
+     * <p>
+     * This endpoint is accessible only to users with any of the following authorities: {@code ${privilege.privilege.delete}}
+     * or {@code ${privilege.privilege.all}}.
+     *
+     * @param id the unique identifier of the privilege to be deleted, provided as a path variable. Must not be null.
+     * @return {@code true} if the privilege was successfully deleted; otherwise, {@code false}.
+     */
     @PreAuthorize("hasAnyAuthority(${privilege.privilege.delete}, ${privilege.privilege.all})")
     @DeleteMapping("/delete/{id}") @Override public @NotNull Boolean delete(@PathVariable @NotNull Long id)
     {
         return super.delete(id);
     }
 
+    /**
+     * Retrieves data for a specific privilege.
+     * <p>
+     * This method processes requests to retrieve information about a privilege specified by the provided {@code id}.
+     * The requested privilege's details are returned in a {@link ResponseEntity} containing the {@link PrivilegeModel}.
+     * This endpoint is restricted to users with appropriate privileges.
+     * <p>
+     * This endpoint is accessible only to users with any of the following authorities: {@code ${privilege.privilege.get}}
+     * or {@code ${privilege.privilege.all}}.
+     *
+     * @param id the unique identifier of the privilege to retrieve, provided as a path variable. Must not be null.
+     * @return a {@link ResponseEntity} containing the {@link PrivilegeModel} of the specified privilege.
+     */
     @PreAuthorize("hasAnyAuthority(${privilege.privilege.get}, ${privilege.privilege.all})") @GetMapping("/get/{id}")
     @Override public @NotNull ResponseEntity<PrivilegeModel> getData(@PathVariable @NotNull Long id)
     {
         return super.getData(id);
     }
 
-    @PreAuthorize("hasAnyAuthority(${privilege.group.privilege.grant}, ${privilege.group.all})") @PostMapping("/{group}/grant")
+    /**
+     * Grants privileges to a specified group.
+     * <p>
+     * This method processes requests to grant one or more privileges to a specified group, identified by the provided
+     * {@code group} ID. The privilege IDs are provided in the request body, and the specified privileges are granted
+     * to the group upon successful processing. This endpoint is restricted to users with appropriate privileges.
+     * <p>
+     * This endpoint is accessible only to users with any of the following authorities: {@code ${privilege.group.privilege.grant}}
+     * or {@code ${privilege.group.all}}.
+     *
+     * @param group      the unique identifier of the target group, provided as a path variable.
+     * @param privileges an array of privilege IDs to grant to the specified group, provided in the request body.
+     *                   Must not be null.
+     */
+    @PreAuthorize("hasAnyAuthority(${privilege.group.privilege.grant}, ${privilege.group.all})")
+    @PostMapping("/{group}/grant")
     public void grantPrivileges(@PathVariable long group, @RequestBody @NotNull Long... privileges)
     {
         PrivilegeEntity[] entities = getEntityService().loadEntityById(privileges).toArray(PrivilegeEntity[]::new);
@@ -51,7 +114,22 @@ public class PrivilegeController extends EntityController<PrivilegeService, Priv
         groupService.loadEntityByIDSafe(group).grantPrivilege(groupService, entities);
     }
 
-    @PreAuthorize("hasAnyAuthority(${privilege.group.privilege.revoke}, ${privilege.group.all})") @PostMapping("/{group}/revoke")
+    /**
+     * Revokes privileges from a specified group.
+     * <p>
+     * This method processes requests to revoke one or more privileges from a specified group, identified by the
+     * provided {@code group} ID. The privilege IDs are provided in the request body, and the specified privileges
+     * are revoked from the group upon successful processing. This endpoint is restricted to users with appropriate privileges.
+     * <p>
+     * This endpoint is accessible only to users with any of the following authorities: {@code ${privilege.group.privilege.revoke}}
+     * or {@code ${privilege.group.all}}.
+     *
+     * @param group      the unique identifier of the target group, provided as a path variable.
+     * @param privileges an array of privilege IDs to revoke from the specified group, provided in the request body.
+     *                   Must not be null.
+     */
+    @PreAuthorize("hasAnyAuthority(${privilege.group.privilege.revoke}, ${privilege.group.all})")
+    @PostMapping("/{group}/revoke")
     public void revokePrivileges(@PathVariable long group, @RequestBody @NotNull Long... privileges)
     {
         GroupService groupService = getEntityService().getGroupService();
