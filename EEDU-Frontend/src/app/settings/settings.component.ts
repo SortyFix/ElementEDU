9bc0ec407a42b9e963659ebe5becaf2410378382
SortyFix <yonasnieder@gmail.com>
2024-10-27 21:34:27 +0100

    Improved settings system scalability
    Added some javadoc as well.

--- EEDU-Frontend/src/app/settings/settings.component.ts
+++ EEDU-Frontend/src/app/settings/settings.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {HttpClient} from "@angular/common/http";
 import {SimpleThemeEntity} from "../theming/simple-theme-entity";
-import {map, Observable} from "rxjs";
+import {forkJoin, Observable} from "rxjs";
 import {FormControl, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatOption} from "@angular/material/autocomplete";
 import {MatSelect} from "@angular/material/select";
 import {AsyncPipe, NgForOf} from "@angular/common";
 import {ThemeEntity} from "../theming/theme-entity";
-import {UserService} from "../user/user.service";
 import {MatButton} from "@angular/material/button";
 import {ThemeService} from "../theming/theme.service";
 
@@ -30,16 +29,16 @@ import {ThemeService} from "../theming/theme.service";
 })
 
 export class SettingsComponent {
-    constructor(public http: HttpClient, public userService: UserService, public themeService: ThemeService) {
+    constructor(public http: HttpClient, public themeService: ThemeService) {
     }
 
     public themes!: Observable<SimpleThemeEntity[]>;
     public selectedTheme!: bigint;
+    public currentThemeName: string = this.getUserData().theme.name;
     themeForm = new FormControl<SimpleThemeEntity | null>(null, Validators.required);
 
     ngOnInit(): void {
         this.themes = this.fetchAllThemes();
-
         this.themeForm.valueChanges.subscribe((selectedTheme) => {
             if (selectedTheme) {
                 this.selectedTheme = selectedTheme.id;
@@ -47,20 +46,69 @@ export class SettingsComponent {
         });
     }
 
-    public saveSettings()
-    {
-        let userData = localStorage.getItem("userData");
+    /**
+     * Parses user data JSON from local storage to a JS object,
+     * providing a convenient way of accessing and modifying specific
+     * certain attributes.
+     *
+     * @returns JavaScript object containing the stored JSON data
+     */
+    public getUserData() {
+        let userData: string | null = localStorage.getItem("userData");
         if(userData)
         {
-            let parsedUserData = JSON.parse(userData);
-            this.setTheme(this.selectedTheme).subscribe(themeEntity => {
-                parsedUserData.theme = themeEntity;
-                localStorage.setItem("userData", JSON.stringify(parsedUserData));
-                location.reload();
-            });
+            return JSON.parse(userData);
         }
     }
 
+    /**
+     * Updates user data by overriding old with new data. This method
+     * automatically stringifies the user data object passed as a parameter.
+     *
+     * @param newUserData JS object containing the updated user data.
+     */
+    public updateUserData(newUserData: any) {
+        localStorage.setItem("userData", JSON.stringify(newUserData));
+    }
+
+    /**
+     * Proxy method for setting changes. Injects necessary user data from
+     * local storage into {@link processSettings()} as a parsed JSON.
+     *
+     * @example
+     * <button mat-button (click)="saveSettings()">Save changes</button>
+     */
+    public saveSettings()
+    {
+        this.processSettings(this.getUserData());
+    }
+
+    /**
+     * Processes user settings and updates the theme in local storage.
+     * IT retrieves the newly selected changes and overrides the affected
+     * data in local storage. If server-based settings are modified, an HTTP request
+     * is initiated to update the backend with the new theme settings using JSON data.
+     *
+     * @param parsedUserData Parsed object representation of the userData JSON retrieved from local storage
+     */
+    public processSettings(parsedUserData: any) {
+        const theme$ = this.setTheme(this.selectedTheme);
+        const observables = [theme$]; // add further observables along the way
+
+        forkJoin(observables).subscribe(([themeEntity]) => {
+            parsedUserData.theme = themeEntity;
+            this.updateUserData(parsedUserData);
+            location.reload();
+        });
+    }
+
+    /**
+     * HTTP request carrying the theme ID as a request body for the backend to process.
+     * Necessary for the theme storage process.
+     *
+     * @param themeId Theme ID to be set.
+     * @returns Observable<ThemeEntity> carrying the full newly selected theme.
+     */
     public setTheme(themeId: bigint) {
         const url: string = `http://localhost:8080/user/me/theme/set`;
         return this.http.put<ThemeEntity>(url, themeId, {
@@ -68,14 +116,15 @@ export class SettingsComponent {
         });
     }
 
+    /**
+     * Fetches all themes as a SimpleThemeEntity array observable. Typically used for
+     * a theme selection dropdown.
+     *
+     * @returns Observable<SimpleThemeEntity[]> containing all themes in a simplified
+     *          id, name format.
+     */
     public fetchAllThemes() : Observable<SimpleThemeEntity[]> {
         const url: string = "http://localhost:8080/user/theme/all";
         return this.http.get<SimpleThemeEntity[]>(url, {withCredentials: true});
-    }
-
-    public listThemes() : Observable<string[]> {
-        return this.themes.pipe(
-            map(themes => themes.map(theme => theme.name))
-        );
     }
 }
