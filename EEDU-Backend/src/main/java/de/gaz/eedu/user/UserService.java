fab93c65b71add06ebb5fc69e88e0aa078ef7333
Ivo Quiring <falk.quiring@gmail.com>
2024-11-03 22:35:02 +0100

    Removed redundant imports
    Signed-off-by: Ivo Quiring <falk.quiring@gmail.com>

--- EEDU-Backend/src/main/java/de/gaz/eedu/user/UserService.java
+++ EEDU-Backend/src/main/java/de/gaz/eedu/user/UserService.java
@@ -32,8 +32,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * This class manages user related tasks.
