9eb22cc1ba5bf5948c77de90c0bb3aa6dac2ca88
Ivo Quiring <falk.quiring@gmail.com>
2024-12-04 21:55:36 +0100

    Strengthened integration with full calendar

--- EEDU-Frontend/src/app/timetable/timetable.component.ts
+++ EEDU-Frontend/src/app/timetable/timetable.component.ts
@@ -1,40 +1,48 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {CourseService} from "../user/courses/course.service";
 import {CourseModel} from "../user/courses/models/course-model";
-import {Calendar, CalendarOptions, EventMountArg} from "@fullcalendar/core";
+import {Calendar, CalendarOptions, EventMountArg, ViewApi} from "@fullcalendar/core";
 import {ScheduledAppointmentModel} from "../user/courses/models/scheduled-appointment-model";
 import {FullCalendarComponent, FullCalendarModule} from "@fullcalendar/angular";
 import {UserService} from "../user/user.service";
 import {RRule} from "rrule";
+import {AccessibilityService} from "../accessibility.service";
 
-import rrulePlugin from '@fullcalendar/rrule'
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridDay from '@fullcalendar/timegrid';
+import rrulePlugin from '@fullcalendar/rrule'
+import {MatCardTitle} from "@angular/material/card";
 
 @Component({
   selector: 'app-timetable',
   standalone: true,
     imports: [
-        FullCalendarModule
+        FullCalendarModule,
+        MatCardTitle
     ],
   templateUrl: './timetable.component.html',
   styleUrl: './timetable.component.scss'
 })
 export class TimetableComponent implements OnInit{
 
+
     @ViewChild('calendar') calendarComponent?: FullCalendarComponent;
     private readonly _calendarOptions: CalendarOptions = {
-        plugins: [dayGridPlugin, rrulePlugin],
-        initialView: 'dayGridMonth',
+        plugins: [dayGridPlugin, timeGridDay, rrulePlugin],
+        initialView: this.accessibilityService.mobile ? 'timeGridDay' : 'dayGridMonth',
         eventTimeFormat: {
             hour: '2-digit', minute: '2-digit', hour12: false
         },
         eventDidMount: (info: EventMountArg): void => {
             info.el.style.color = this.userService.getUserData.theme.getTextColor('widget', false);
         },
-        events: []
+        windowResize: (arg: {view: ViewApi}): void => {
+            arg.view.calendar.changeView(this.accessibilityService.mobile ? 'timeGridDay' : 'dayGridMonth')
+        },
+        events: [],
     };
 
-    constructor(private userService: UserService, private courseService: CourseService) {}
+    constructor(private userService: UserService, private accessibilityService: AccessibilityService, private courseService: CourseService) {}
 
     ngOnInit(): void {
         this.courseService.fetchCourses().subscribe((courses: CourseModel[]): void => {
