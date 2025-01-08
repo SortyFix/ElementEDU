9653e7138ba8bb34a9760540a44039b429cab7ce
SortyFix <yonasnieder@gmail.com>
2025-01-08 18:35:13 +0100

    Implemented component outlet for cards

--- EEDU-Frontend/src/app/dashboard/dashboard.component.ts
+++ EEDU-Frontend/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import {ThemeService} from "../theming/theme.service";
 import {UserService} from "../user/user.service";
 import {CdkDragDrop} from "@angular/cdk/drag-drop";
+import {AppointmentCardComponent} from "./appointment-card/appointment-card.component";
+import {HomeworkCardComponent} from "./homework-card/homework-card.component";
+import {NewsComponent} from "../news/news.component";
+import {NewsCardComponent} from "./news-card/news-card.component";
+import {ChatCardComponent} from "./chat-card/chat-card.component";
 
 @Component({
   selector: 'app-dashboard',
@@ -13,10 +18,10 @@ export class DashboardComponent {
     }
 
     cards = [
-        { title: 'Timetable', content: 'This is the content of card 1.' },
-        { title: 'Homework', content: 'This is the content of card 2.' },
-        { title: 'Latest news', content: 'This is the content of card 3.' },
-        { title: 'Latest contacts', content: 'This is the content of card 4.' }
+        { title: 'Next appointments', content: 'This is the content of card 1.', component: AppointmentCardComponent },
+        { title: 'Homework', content: 'This is the content of card 2.', component: HomeworkCardComponent },
+        { title: 'Latest news', content: 'This is the content of card 3.', component: NewsCardComponent },
+        { title: 'Latest contacts', content: 'This is the content of card 4.', component: ChatCardComponent }
     ];
 
     public get user() {
---
+++ EEDU-Frontend/src/app/dashboard/homework-card/homework-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homework-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './homework-card.component.html',
+  styleUrl: './homework-card.component.scss'
+})
+export class HomeworkCardComponent {
+
+}
