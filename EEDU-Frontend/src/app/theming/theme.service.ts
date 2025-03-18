a03f30505f4137c184f5d6ddb307be4d97b8b890
SortyFix <yonasnieder@gmail.com>
2025-03-18 19:07:43 +0100

    Removed BS

--- EEDU-Frontend/src/app/theming/theme.service.ts
+++ EEDU-Frontend/src/app/theming/theme.service.ts
@@ -5,11 +5,7 @@ import {ThemeModel} from "./theme-model";
 @Injectable({
     providedIn: 'root'
 })
-/**
- * This ThemeService provides methods to retrieve theme-related information
- * for UI elements. Apart from getter methods, this service also includes text color
- * logic based on the luminance of the given background for better readability.
- */
+
 export class ThemeService {
     constructor(public userService: UserService) { }
 
