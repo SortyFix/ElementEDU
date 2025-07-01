cfa8f113f553243b4429bc238dc658c9b72f3759
Ivo Quiring <falk.quiring@gmail.com>
2025-07-02 00:39:53 +0200

    Added environment configuration for development and production

---
+++ EEDU-Frontend/src/environment/environment.production.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    timeZone: 'Europe/Berlin',
+    backendUrl: 'http://localhost:8080'  // This could be adjusted to another backend in production
+};
---
+++ EEDU-Frontend/src/environment/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    timeZone: 'Europe/Berlin',
+    backendUrl: 'http://localhost:8080/api/v1',
+    websocketUrl: 'ws://localhost:8080/ws-endpoint'
+};
